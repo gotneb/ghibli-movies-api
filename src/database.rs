d6db47f1bddf4e236509a93057fa046b4ad3c712
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-10-08 17:59:56 -0400

    refactor: using `async` MongoDB instead of blocking

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io;
 
-use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion, FindOptions}, sync::{Client, Cursor}, IndexModel};
+use mongodb::{Client, Cursor, bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion}};
 use bson::{Document, Bson};
 
 use crate::movie::Movie;
@@ -14,12 +14,12 @@ pub struct Database {
 
 impl Database {
     /// Creates a new instance of [Database]
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
         let uri = env::var("MONGODB_URI").expect("MONGODB_URI not specified!");
         let db_name = env::var("DATABASE_NAME").expect("DATABASE_NAME not specified!");
         let coll_name = env::var("COLLECTION_NAME").expect("COLLECTION_NAME not specified!");
 
-        let mut client_options = ClientOptions::parse(uri).unwrap();
+        let mut client_options = ClientOptions::parse(uri).await.unwrap();
         let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
         client_options.server_api = Some(server_api);
 
@@ -33,21 +33,26 @@ impl Database {
     }
 
     /// Searches for a given title into database
-    pub fn search(&self, title: &String) -> Option<Vec<Movie>> {
+    pub async fn search(&self, title: &String) -> Option<Vec<Movie>> {
         // This query only works if an index is already created in MongoDB
         // Otherwise `find` method panics!
         let query = doc! { "$text": { "$search": title, } };
     
-        let cursor: Cursor<Document> = self.client
+        let mut cursor: Cursor<Document> = self.client
             .database(&self.db_name)
             .collection(&self.coll_name)
             .find(Some(query), None)
+            .await
             .unwrap();
     
         let mut movies = Vec::new();
 
-        for result in cursor {
-            match result {
+        loop {
+            if !cursor.advance().await.unwrap() {
+                break
+            }
+            
+            match cursor.deserialize_current() {
                 Ok(doc) => {
                     if let Ok(movie) = bson::from_bson::<Movie>(Bson::Document(doc)) {
                         movies.push(movie);
@@ -56,11 +61,11 @@ impl Database {
                 Err(_) => (),
             }
         }
-    
+
         Some(movies)
     }
 
-    fn add(&self, movie: &Movie) -> Result<(), io::Error> {
+    pub async fn add(&self, movie: &Movie) -> Result<(), io::Error> {
         let movie = bson::to_document(&movie);
 
         match movie {
@@ -69,6 +74,7 @@ impl Database {
                 .database(&self.db_name)
                 .collection::<Document>(&self.coll_name)
                 .insert_one(movie, None)
+                .await
                 .expect("Error adding movie!");
 
                 Ok(())
